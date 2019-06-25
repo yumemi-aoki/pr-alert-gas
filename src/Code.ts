12ce333027af92c9b3f287219fc2069b9ee7e661
yumemi-aoki <k_aoki@yumemi.co.jp>
2019-06-25 19:08:22 +0900

    fix: Slackにテスト投稿

--- src/Code.ts
+++ src/Code.ts
@@ -1,8 +1,28 @@
-function main() {
-  Logger.log('Hello World');
+function main() {}
+
+/**
+ * @param req HttpRequest
+ */
+function doPost(req) {
+  const data = JSON.parse(req.postData.getDataAsString());
+  if (data.pull_request) {
+    slackPost(data.pull_request.url);
+  }
 }
 
-function doPost(e) {
-  const data = JSON.parse(e.postData.getDataAsString());
-  Logger.log(data);
+function slackPost(message) {
+  const appConfig = PropertiesService.getScriptProperties();
+  const postUrl = appConfig.getProperty("SLACK_WEBHOOK_URL");
+  const send = {
+    username: "test",
+    icon_emoji: ":gueee:",
+    text: "test message" + message
+  };
+  const options = {
+    method: "post",
+    contentType: "application/json",
+    payload: JSON.stringify(send)
+  };
+
+  UrlFetchApp.fetch(postUrl, options);
 }
